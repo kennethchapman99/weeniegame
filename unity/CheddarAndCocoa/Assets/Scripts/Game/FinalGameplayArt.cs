044ee091fdf86a1df8355ab1d3f6debb861b8689
Kenneth Chapman <kenneth.chapman@gmail.com>
2026-06-18 21:31:58 -0400

    Expand final gameplay art paths

--- unity/CheddarAndCocoa/Assets/Scripts/Game/FinalGameplayArt.cs
+++ unity/CheddarAndCocoa/Assets/Scripts/Game/FinalGameplayArt.cs
@@ -16,8 +16,14 @@ namespace CheddarAndCocoa.Game
             {
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.Squirrel:
                     return Root + "/Characters/Squirrel/squirrel_idle";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.SquirrelSteal:
+                    return Root + "/Characters/Squirrel/squirrel_steal";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.SquirrelScared:
+                    return Root + "/Characters/Squirrel/squirrel_scared";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.EagleThreat:
                     return Root + "/Characters/Eagle/eagle_threat";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.PredatorAttack:
+                    return Root + "/Characters/Eagle/eagle_attack";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.CoyoteThreat:
                     return Root + "/Characters/Coyote/coyote_threat";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.BackyardBush:
@@ -26,16 +32,30 @@ namespace CheddarAndCocoa.Game
                     return Root + "/Props/Backyard/fence_section";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.BackyardRock:
                     return Root + "/Props/Backyard/rock";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.GrassPatch:
+                    return Root + "/Props/Backyard/grass_patch";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.DigSpot:
+                    return Root + "/Props/Backyard/dig_spot";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.DogBowl:
+                    return Root + "/Props/Mission/dog_bowl";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.BarkBurst:
                     return Root + "/VFX/bark_burst";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.BarkRing:
+                    return Root + "/VFX/bark_ring";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.PickupSparkle:
                     return Root + "/VFX/pickup_sparkle";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.SuccessPop:
                     return Root + "/VFX/success_pop";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.WarningAlert:
                     return Root + "/VFX/warning_alert";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.RescueBurst:
+                    return Root + "/VFX/rescue_burst";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.FailPuff:
+                    return Root + "/VFX/fail_puff";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.RopeToy:
                     return Root + "/Props/Mission/rope_tug";
+                case RuntimeArtSpriteFactory.RuntimeSpriteId.RopeComplete:
+                    return Root + "/Props/Mission/rope_complete";
                 case RuntimeArtSpriteFactory.RuntimeSpriteId.WeenieCollectible:
                     return Root + "/Props/Mission/weenie_collectible";
                 default:
